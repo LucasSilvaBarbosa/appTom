5c958264831b6e7a6b7789cb852d1dd78ddfcf7d
LucasSilvaBarbosa <39389553+LucasSilvaBarbosa@users.noreply.github.com>
2018-06-15 07:54:33 -0300

    Classes Model

---
+++ app/src/test/java/Model/Usuario.java
@@ -0,0 +1,93 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Usuario {
+    private String email;
+    private int id;
+    private String login;
+    private String nome;
+    private String senha;
+    private String token;
+    private List<Voo> voos;
+    private CartaoCredito cartao;
+
+    public Usuario() {
+    }
+
+    public Usuario(String email, int id, String login, String nome, String senha, String token, CartaoCredito cartao) {
+        this.email = email;
+        this.id = id;
+        this.login = login;
+        this.nome = nome;
+        this.senha = senha;
+        this.token = token;
+        this.voos = new ArrayList<>();
+        this.cartao = cartao;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public List<Voo> getVoos() {
+        return voos;
+    }
+
+    public void setVoos(List<Voo> voos) {
+        this.voos = voos;
+    }
+
+    public CartaoCredito getCartao() {
+        return cartao;
+    }
+
+    public void setCartao(CartaoCredito cartao) {
+        this.cartao = cartao;
+    }
+}
---
+++ app/src/test/java/Model/Voo.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class Voo {
+    private int id;
+    private Origem origem;
+    private Destino destino;
+    private String datavoo;
+    private double valor;
+    private Aviao aviao;
+}
