e20bc02d7eed483de6a381e84c6a4e1ef2acaa5e
LucasSilvaBarbosa <39389553+LucasSilvaBarbosa@users.noreply.github.com>
2018-06-16 03:36:31 -0300

    Novo Usuario Service

--- app/src/main/java/com/example/lucas/apptom/LoginActivity.java
+++ app/src/main/java/com/example/lucas/apptom/LoginActivity.java
@@ -2,12 +2,34 @@ package com.example.lucas.apptom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ImageButton imgLogar,imgNovoUsuario;
+    EditText edtlogin,senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+        binding();
+
+        imgNovoUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void binding() {
+        imgLogar = findViewById(R.id.imgLogar);
+        imgNovoUsuario = findViewById(R.id.imgNovoUsuario);
+        edtlogin = findViewById(R.id.edtLogin);
+        senha = findViewById(R.id.edtSenha);
     }
 }
