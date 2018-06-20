327937008a297e21f37ea78808cec5163ffd2c58
LucasSilvaBarbosa <39389553+LucasSilvaBarbosa@users.noreply.github.com>
2018-06-20 08:19:54 -0300

    Tela Lista Voo

---
+++ app/src/main/java/com/example/lucas/apptom/ListaVoosActivity.java
@@ -0,0 +1,13 @@
+package com.example.lucas.apptom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaVoosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_voos);
+    }
+}
